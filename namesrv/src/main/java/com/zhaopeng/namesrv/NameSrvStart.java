dc22ae38c491578ec06eeeaf2a6295c02d423915
zhaopeng <zp6282154@163.com>
2017-04-06 00:17:42 +0800

    msic：common model

--- namesrv/src/main/java/com/zhaopeng/namesrv/NameSrvStart.java
+++ namesrv/src/main/java/com/zhaopeng/namesrv/NameSrvStart.java
@@ -1,9 +1,22 @@
 package com.zhaopeng.namesrv;
 
+import com.zhaopeng.remoting.netty.NettyServerConfig;
+
 /**
  * Created by zhaopeng on 2017/4/5.
- *
+ * <p>
  * 启动消息队列的服务发现和注册
  */
 public class NameSrvStart {
+
+    public static void main(String args[]) {
+        startUp();
+    }
+
+    private static void startUp() {
+        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        nettyServerConfig.setPort(9876);
+
+    }
+
 }
