6aabcf06612f0df9ff8bf9c8bbb8b6ffd5770176
zhaopeng <zp6282154@163.com>
2017-08-02 23:37:27 +0800

    DiskMessageStore

--- store/src/main/java/com/zhaopeng/store/commit/CommitLog.java
+++ store/src/main/java/com/zhaopeng/store/commit/CommitLog.java
@@ -1,7 +1,13 @@
 package com.zhaopeng.store.commit;
 
+import com.zhaopeng.remoting.common.ServiceThread;
 import com.zhaopeng.store.entity.MessageExtBrokerInner;
 import com.zhaopeng.store.entity.PutMessageResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhaopeng on 2017/7/27.
@@ -9,6 +15,10 @@ import com.zhaopeng.store.entity.PutMessageResult;
 public class CommitLog {
     private volatile long confirmOffset = -1L;
     private volatile long beginTimeInLock = 0;
+
+
+    private final FlushCommitLogService flushCommitLogService;
+
 
     public long getConfirmOffset() {
         return confirmOffset;
@@ -26,10 +36,158 @@ public class CommitLog {
         this.beginTimeInLock = beginTimeInLock;
     }
 
+
+    public CommitLog() {
+        this.flushCommitLogService = new GroupCommitService();
+    }
+
     public PutMessageResult putMessage(final MessageExtBrokerInner msg) {
 
 
         return null;
+    }
+
+    abstract class FlushCommitLogService extends ServiceThread {
+    }
+
+    class FlushRealTimeService extends FlushCommitLogService {
+        private static final int RetryTimesOver = 3;
+        private long lastFlushTimestamp = 0;
+        private long printTimes = 0;
+
+
+        public void run() {
+/*            CommitLog.log.info(this.getServiceName() + " service started");
+
+            while (!this.isStoped()) {
+                boolean flushCommitLogTimed = CommitLog.this.defaultMessageStore.getMessageStoreConfig().isFlushCommitLogTimed();
+
+                int interval = CommitLog.this.defaultMessageStore.getMessageStoreConfig().getFlushIntervalCommitLog();
+                int flushPhysicQueueLeastPages = CommitLog.this.defaultMessageStore.getMessageStoreConfig().getFlushCommitLogLeastPages();
+
+                int flushPhysicQueueThoroughInterval =
+                        CommitLog.this.defaultMessageStore.getMessageStoreConfig().getFlushCommitLogThoroughInterval();
+
+                boolean printFlushProgress = false;
+
+                // Print flush progress
+                long currentTimeMillis = System.currentTimeMillis();
+                if (currentTimeMillis >= (this.lastFlushTimestamp + flushPhysicQueueThoroughInterval)) {
+                    this.lastFlushTimestamp = currentTimeMillis;
+                    flushPhysicQueueLeastPages = 0;
+                    printFlushProgress = ((printTimes++ % 10) == 0);
+                }
+
+                try {
+                    if (flushCommitLogTimed) {
+                        Thread.sleep(interval);
+                    } else {
+                        this.waitForRunning(interval);
+                    }
+
+                    if (printFlushProgress) {
+                        this.printFlushProgress();
+                    }
+
+                    CommitLog.this.mapedFileQueue.commit(flushPhysicQueueLeastPages);
+                    long storeTimestamp = CommitLog.this.mapedFileQueue.getStoreTimestamp();
+                    if (storeTimestamp > 0) {
+                        CommitLog.this.defaultMessageStore.getStoreCheckpoint().setPhysicMsgTimestamp(storeTimestamp);
+                    }
+                } catch (Exception e) {
+                    CommitLog.log.warn(this.getServiceName() + " service has exception. ", e);
+                    this.printFlushProgress();
+                }
+            }
+
+            // Normal shutdown, to ensure that all the flush before exit
+            boolean result = false;
+            for (int i = 0; i < RetryTimesOver && !result; i++) {
+                result = CommitLog.this.mapedFileQueue.commit(0);
+                CommitLog.log.info(this.getServiceName() + " service shutdown, retry " + (i + 1) + " times " + (result ? "OK" : "Not OK"));
+            }
+
+            this.printFlushProgress();
+
+            CommitLog.log.info(this.getServiceName() + " service end");*/
+        }
+
+
+        @Override
+        public String getServiceName() {
+            return FlushCommitLogService.class.getSimpleName();
+        }
+
+
+        private void printFlushProgress() {
+            // CommitLog.log.info("how much disk fall behind memory, "
+            // + CommitLog.this.mapedFileQueue.howMuchFallBehind());
+        }
+
+
+        @Override
+        public long getJointime() {
+            return 1000 * 60 * 5;
+        }
+    }
+
+    public static class GroupCommitRequest {
+        private final long nextOffset;
+        private final CountDownLatch countDownLatch = new CountDownLatch(1);
+        private volatile boolean flushOK = false;
+
+
+        public GroupCommitRequest(long nextOffset) {
+            this.nextOffset = nextOffset;
+        }
+
+
+        public long getNextOffset() {
+            return nextOffset;
+        }
+
+
+        public void wakeupCustomer(final boolean flushOK) {
+            this.flushOK = flushOK;
+            this.countDownLatch.countDown();
+        }
+
+
+        public boolean waitForFlush(long timeout) {
+            try {
+                this.countDownLatch.await(timeout, TimeUnit.MILLISECONDS);
+                return this.flushOK;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+
+    class GroupCommitService extends FlushCommitLogService {
+        private volatile List<GroupCommitRequest> requestsWrite = new ArrayList<>();
+        private volatile List<GroupCommitRequest> requestsRead = new ArrayList<>();
+
+        @Override
+        protected void onWaitEnd() {
+
+        }
+
+        @Override
+        public String getServiceName() {
+            return GroupCommitService.class.getSimpleName();
+        }
+
+
+        @Override
+        public long getJointime() {
+            return 1000 * 60 * 5;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 
 
--- store/src/main/java/com/zhaopeng/store/commit/MapedFile.java
+++ store/src/main/java/com/zhaopeng/store/commit/MapedFile.java
@@ -1,7 +1,28 @@
 package com.zhaopeng.store.commit;
 
+import java.io.File;
+import java.nio.channels.FileChannel;
+
 /**
  * Created by zhaopeng on 2017/8/1.
  */
 public class MapedFile {
+
+    private final String fileName;
+
+    private final long fileFromOffset;
+
+    private final int fileSize;
+
+    private final File file;
+
+    private FileChannel fileChannel;
+
+
+    public MapedFile(String fileName, long fileFromOffset, int fileSize, File file) {
+        this.fileName = fileName;
+        this.fileFromOffset = fileFromOffset;
+        this.fileSize = fileSize;
+        this.file = file;
+    }
 }
